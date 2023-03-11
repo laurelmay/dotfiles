017853f14a2a44703d506c2179027dd6b1426242
Kyle Laker <kyle@laker.email>
2023-03-11 14:35:01 -0500

    chore(vim): improve file explorer config

--- roles/vim/files/after/plugin/neo-tree.lua
+++ roles/vim/files/after/plugin/neo-tree.lua
@@ -4,6 +4,8 @@ require "neo-tree".setup {
   close_if_last_window = true,
   filesystem = {
     follow_current_file = true,
+    group_empty_dirs = true,
+    use_libuv_file_watcher = true,
     filtered_items = {
       always_show = { ".github", ".projenrc.js", ".projenrc.ts" },
       never_show = { ".git", 'node_modules' },
